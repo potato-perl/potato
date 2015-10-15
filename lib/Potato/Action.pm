0269f5f71bb51d5f08493c9542a77ec4829b5cdd
Graeme Lawton <graeme.lawton@setfiremedia.com>
2015-10-15 21:11:38 +0100

    Added attribute parsing.

--- lib/Potato/Action.pm
+++ lib/Potato/Action.pm
@@ -6,13 +6,33 @@ has subname => (
     is  => 'ro',
     isa => 'Str',
 );
+
 has attrs => (
     is  => 'ro',
-    isa => 'ArrayRef',
+    writer => '_attrs',
 );
+
 has classname => (
     is  => 'ro',
     isa => 'Str',
 );
+
+sub BUILD {
+    my ($self, $args) = @_;
+	$self->_attrs( $self->_parse_attrs($args->{attrs}) );
+}
+
+sub _parse_attrs {
+	my ( $self, $raw_attrs ) = @_;
+	my $attrs = {};
+
+	foreach my $attr (@{$raw_attrs} ) {
+		if ( my ( $key, $value ) = ( $attr =~ /^(.*?)(?:\(\s*(.*?)\s*\))?$/ ) ){
+			push @{$attrs->{$key}}, $value;
+		}
+	}
+
+	return $attrs;
+}
 
 __PACKAGE__->meta->make_immutable;
--- t/01-dispatcher.t
+++ t/01-dispatcher.t
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests => 4;
+use Test::More tests => 6;
 
 my $ok;
 END { BAIL_OUT "Could not load all modules" unless $ok }
@@ -17,3 +17,10 @@ ok 2, 'new worked';
 
 is scalar @{$app->controllers}, 1, '1 controller';
 is scalar @{$app->controllers->[0]->actions}, 3, '3 actions';
+
+#Attributes
+{
+	my $attrs = $app->controllers->[0]->actions->[2	]->attrs;
+	is scalar(keys %{$attrs}), 2, "Action has 2 attributes";
+	is scalar(@{$attrs->{SomeAttr}}), 3, "Attribute has 3 values";
+}
